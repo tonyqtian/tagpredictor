f3234ff4ad1acfcad2fac467b8d8aa5b1c013cc2
tonyqtian <tonyqtian@gmail.com>
2017-03-19 14:21:05 +0800

    fix prama typo

--- src/train_helper.py
+++ src/train_helper.py
@@ -12,16 +12,16 @@ from time import sleep
 #
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--train-path", dest="train_path", type=str, metavar='<str>', default='data/train_s4.tsv', help="The path to the training set")
-parser.add_argument("--test-path", dest="test_path", type=str, metavar='<str>', default='data/test_s4.tsv', help="The path to the test set")
+parser.add_argument("--train-path", dest="train_path", type=str, metavar='<str>', default='../data/robotics.csv', help="The path to the training set")
+parser.add_argument("--test-path", dest="test_path", type=str, metavar='<str>', default='../data/physics_sample.csv', help="The path to the test set")
 parser.add_argument("--out-dir", dest="out_dir_path", type=str, metavar='<str>', default='output', help="The path to the output directory")
 parser.add_argument("--model-type", dest="model_type", type=str, metavar='<str>', default='cls', help="Model type (cls|reg) (default=cls)")
 parser.add_argument("--optimizer", dest="optimizer", type=str, metavar='<str>', default='rmsprop', help="Optimization algorithm (rmsprop|sgd|adagrad|adadelta|adam|adamax) (default=rmsprop)")
 parser.add_argument("--loss", dest="loss", type=str, metavar='<str>', default='mse', help="Loss function (mse|mae|cnp|hng) (default=mse) set to cnp if cls model")
-parser.add_argument("--embding-dim", dest="embd_dim", type=int, metavar='<int>', default=50, help="Embeddings dimension (default=50)")
+parser.add_argument("--embedding-dim", dest="embd_dim", type=int, metavar='<int>', default=50, help="Embeddings dimension (default=50)")
 parser.add_argument("--cnn-kernel", dest="cnn_dim", type=int, metavar='<int>', default=0, help="CNN output dimension. '0' means no CNN layer (default=0)")
-parser.add_argument("--cnnwin", dest="cnn_window_size", type=int, metavar='<int>', default=3, help="CNN window size. (default=3)")
-parser.add_argument("--rnndim", dest="rnn_dim", type=int, metavar='<int>', default=0, help="RNN dimension. '0' means no RNN layer (default=0)")
+parser.add_argument("--cnn-win", dest="cnn_window_size", type=int, metavar='<int>', default=3, help="CNN window size. (default=3)")
+parser.add_argument("--rnn-dim", dest="rnn_dim", type=int, metavar='<int>', default=0, help="RNN dimension. '0' means no RNN layer (default=0)")
 parser.add_argument("--train-batch-size", dest="train_batch_size", type=int, metavar='<int>', default=8, help="Train Batch size (default=8)")
 parser.add_argument("--eval-batch-size", dest="eval_batch_size", type=int, metavar='<int>', default=8, help="Eval Batch size (default=8)")
 parser.add_argument("--dropout", dest="dropout_prob", type=float, metavar='<float>', default=0.4, help="The dropout probability. To disable, give a negative number (default=0.4)")
@@ -36,7 +36,7 @@ parser.add_argument("--onscreen", dest="onscreen", action='store_true', help="Sh
 parser.add_argument("--earlystop", dest="earlystop", type=int, metavar='<int>', default=4, help="Use early stop")
 parser.add_argument("--verbose", dest="verbose", type=int, metavar='<int>', default=1, help="Show training process bar during train and val")
 parser.add_argument("--valid-split", dest="valid_split", type=float, metavar='<float>', default=0.2, help="Split validation set from training set (default=0.0)")
-parser.add_argument("--rnn-optimization", dest="rnn_opt", type=str, metavar='<str>', default='gpu', help="RNN consume_less (cpu|mem|gpu) (default=gpu)")
+parser.add_argument("--mem-opt", dest="rnn_opt", type=str, metavar='<str>', default='gpu', help="RNN consume_less (cpu|mem|gpu) (default=gpu)")
 args = parser.parse_args()
 
 from processing import train
